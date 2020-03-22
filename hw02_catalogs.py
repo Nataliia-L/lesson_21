32dd6f7a1f21f3384b15a74de77c9c8604c106a1
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2020-03-22 16:28:26 +0200

    Create hw02_catalogs.py

---
+++ hw02_catalogs.py
@@ -0,0 +1,3 @@
+
+d_currency = {980: "UAH", 643: "RUB", 840: "USD", 943: "EUR"}
+d_country = {"ua": ("Ukraine", 980), "ru": ("Russia", 643), "us": ("USA", 840), "uk": ("United Kindom", 943), "pl": ("Poland", 943), "es": ("Spain", 943)}
